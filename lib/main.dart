ca654474d2b5de0e5f55a27ff40fd8897f88e3a6
Rafael Rezende <rrsc1996@hotmail.com>
2022-09-01 16:26:37 -0300

    Adicionado a row com os botões,

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false ,
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
@@ -19,14 +19,22 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  void decrement() {
+    debugPrint('Saiu');
+  }
+
+  void increment() {
+    debugPrint('Entrou');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text(
+        children: [
+          const Text(
             'Pode entrar!!!!',
             style: TextStyle(
               fontSize: 35,
@@ -34,12 +42,50 @@ class HomePage extends StatelessWidget {
               fontWeight: FontWeight.w700,
             ),
           ),
-          Text(
+          const Text(
             '0',
             style: TextStyle(
               fontSize: 100,
               color: Colors.white,
             ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: decrement,
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                  primary: Colors.red,
+                ),
+                child: const Text(
+                  'Saiu',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 30,
+              ),
+              TextButton(
+                onPressed: increment,
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                  primary: Colors.green,
+                ), //TextButton styleFrom
+                child: const Text(
+                  'Entrou',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ], //Children
           ),
         ],
       ),
