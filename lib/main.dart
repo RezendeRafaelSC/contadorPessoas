d986c5de614192b78ded173dbba6178a24bbbcae
Rafael Rezende <rrsc1996@hotmail.com>
2022-09-02 16:13:25 -0300

    Terceiro dia de projeto e conclusão!

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,119 @@ void main() {
   runApp(const MyApp());
 }
 
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int contadorPessoas = 0;
+
+  void decrement() {
+    setState(() {
+      contadorPessoas--;
+    });
+    print(contadorPessoas);
+  }
+
+  void increment() {
+    setState(() {
+      contadorPessoas++;
+    });
+    print(contadorPessoas);
+  }
+
+  bool get isEmpty => contadorPessoas == 0;
+  bool get isFull => contadorPessoas == 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/image.jpg'), fit: BoxFit.cover),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              isFull ? 'Está lotado!' : 'Pode entrar!!!!',
+              style: const TextStyle(
+                fontSize: 35,
+                color: Colors.black,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            Text(
+              contadorPessoas.toString(),
+              style: const TextStyle(
+                fontSize: 100,
+                color: Colors.black,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: isEmpty ? null : decrement,
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        isEmpty ? Colors.grey.withOpacity(0.2) : Colors.grey,
+                    fixedSize: const Size(100, 80),
+                    primary: Colors.red,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)),
+                  ),
+                  child: const Text(
+                    'Saiu',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 30,
+                ),
+                TextButton(
+                  onPressed: isFull ? null : increment,
+                  style: TextButton.styleFrom(
+                    backgroundColor:
+                        isFull ? Colors.grey.withOpacity(0.2) : Colors.grey,
+                    fixedSize: const Size(100, 80),
+                    primary: Colors.green,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)),
+                  ), //TextButton styleFrom
+
+                  child: const Text(
+                    'Entrou',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ], //Children
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -12,83 +125,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  void decrement() {
-    debugPrint('Saiu');
-  }
-
-  void increment() {
-    debugPrint('Entrou');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Pode entrar!!!!',
-            style: TextStyle(
-              fontSize: 35,
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-          const Text(
-            '0',
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.white,
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100, 100),
-                  primary: Colors.red,
-                ),
-                child: const Text(
-                  'Saiu',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 30,
-              ),
-              TextButton(
-                onPressed: increment,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100, 100),
-                  primary: Colors.green,
-                ), //TextButton styleFrom
-                child: const Text(
-                  'Entrou',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-            ], //Children
-          ),
-        ],
-      ),
     );
   }
 }
